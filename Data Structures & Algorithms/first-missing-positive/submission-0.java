8f92946bda48fa314468b8a1b243eaa13cfcf098
Rahul Panda <rahulpanda7851@gmail.com>
2026-04-29 12:18:01 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    
+    public int firstMissingPositive(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i : nums){
+            set.add(i);
+        }
+
+        int i=1;  
+
+        while(true){
+
+            if(!set.contains(i)){
+                return i;
+            }
+
+            i++;
+        }
+
+    }
+}
